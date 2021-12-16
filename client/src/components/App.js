4179462626b0afa137d3c99dcdbbd45e2642dc75
VinceBaylon <vincentbaylon@gmail.com>
2021-12-15 22:02:36 -0800

    Added tailwind

---
+++ client/src/components/App.js
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className='h-screen w-screen bg-gray-400 flex flex-column justify-center align-center'>
+			<h1 className='text-red-400'>HELLO</h1>
+		</div>
+	)
+}
+
+export default App
